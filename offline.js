13b6e3938a270f42efa0ebabe657fa21dc1222ef
ArthurVianaCreator <165707712+ArthurVianaCreator@users.noreply.github.com>
2025-11-27 14:27:24 -0300

    a

---
+++ offline.js
@@ -0,0 +1,29 @@
+﻿{
+	"version": 1764264346,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/player-sheet0.png",
+		"images/camera-sheet0.png",
+		"images/rex1-sheet0.png",
+		"images/rex1-sheet1.png",
+		"images/ia_rex-sheet0.png",
+		"images/grama_bloco.png",
+		"images/fundo.png",
+		"images/bloco2.png",
+		"images/grama_terra_bloco.png",
+		"images/spikes-sheet0.png",
+		"images/grama-sheet0.png",
+		"images/fadein.png",
+		"images/sprite-sheet0.png",
+		"images/heart_hud-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
